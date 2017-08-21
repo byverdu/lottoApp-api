d4f1febf29b78740f23a0cddcf503eb88592ea36
albert Byverdu :) <byverdu@gmail.com>
2017-08-21 23:41:45 +0100

    solved mongoose  deprecated props

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   server: {
     host: 'localhost',
-    port: 3800
+    port: 3000
   },
   database: {
     development: 'mongodb://localhost/lottoApp',
--- database/index.js
+++ database/index.js
@@ -5,6 +5,8 @@ const mongoose = require( 'mongoose' );
 const uriUtil = require( 'mongodb-uri' );
 let dbURL;
 
+mongoose.Promise = Promise;
+
 if ( process.env.NODE_ENV === 'development' ) {
   dbURL = config.database.development;
 }
@@ -14,11 +16,9 @@ if ( process.env.NODE_ENV === 'production' ) {
 }
 
 const mongooseUri = uriUtil.formatMongoose( dbURL ); // formatting url for better parsing
-mongoose.connect( mongooseUri );
-const db = mongoose.connection;
-db.on( 'error', console.error.bind( console, 'connection error' ));
-db.once( 'open', () => {
-    console.log( 'Connection with database succeeded.' );
+const dbPromise = mongoose.connect( mongooseUri, {
+  useMongoClient: true
 });
+
 exports.Mongoose = mongoose;
-exports.db = db;
+exports.dbPromise = dbPromise;
